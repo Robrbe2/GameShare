fc48a8263c9afbab1786ce6532efab9d47925c79
Jorn De Wolf <jorn@aliso.be>
2023-03-22 15:11:36 +0100

    Mod achievement to include ID

--- PancakeClicker/PancakeLibrary/Achievement.cs
+++ PancakeClicker/PancakeLibrary/Achievement.cs
@@ -8,6 +8,7 @@ namespace PancakeLibrary
 {
     public class Achievement
     {
+        public int id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
